de80cfcc3450a53c3bb448aed3a71b9b427f5620
Basil L'HÔTE <blhote@ankama.com>
2024-10-01 09:21:13 +0200

    Populate test

---
+++ populate.js
@@ -0,0 +1,79 @@
+async function populateSets() {
+    let hasMore = true;
+    let page = 1;
+
+    while (hasMore) {
+        const response = await fetch(`${config.BASE_URL}/sets?page=${page}`, {
+            headers: { 'X-Api-Key': config.API_KEY }
+        });
+        const data = await response.json();
+
+        if (data.data.length === 0) {
+            hasMore = false;
+            break;
+        }
+
+        const sets = data.data.map(set => ({
+            set_id: set.id,
+            set_name: set.name,
+            releaseDate: set.releaseDate,
+            printedTotal: set.printedTotal,
+            total: set.total
+        }));
+
+        const { error } = await supabase.from('sets').insert(sets);
+        if (error) {
+            console.error("Erreur lors de l'insertion des sets : ", error.message);
+            return;
+        }
+
+        page++;
+    }
+    console.log("Peuplement des sets terminé.");
+    populateCards();
+}
+
+async function populateCards() {
+    let hasMore = true;
+    let page = 1;
+
+    while (hasMore) {
+        const response = await fetch(`${config.BASE_URL}/cards?page=${page}`, {
+            headers: { 'X-Api-Key': config.API_KEY }
+        });
+        const data = await response.json();
+
+        if (data.data.length === 0) {
+            hasMore = false;
+            break;
+        }
+
+        const cards = data.data.map(card => ({
+            id: card.id,
+            name: card.name,
+            rarity: card.rarity || null,
+            value: card.prices ? card.prices.market : 0,
+            image_url: card.images.large,
+            artist: card.artist || null,
+            nationalPokedexNumber: card.nationalPokedexNumbers ? card.nationalPokedexNumbers[0] : null,
+            supertype: card.supertype,
+            subtypes: card.subtypes ? card.subtypes.join(", ") : null,
+            hp: card.hp || null,
+            types: card.types ? card.types.join(", ") : null,
+            image_small: card.images.small,
+            set_id: card.set.id // Associer la carte à son set
+        }));
+
+        const { error } = await supabase.from('cards').insert(cards);
+        if (error) {
+            console.error("Erreur lors de l'insertion des cartes : ", error.message);
+            return;
+        }
+
+        page++;
+    }
+    console.log("Peuplement des cartes terminé.");
+}
+
+
+populateSets();
--- supabase.js
+++ supabase.js
@@ -1,9 +1,11 @@
 // supabase.js
 
-// Configuration Supabase
+// Configuration Supabase et TGC
 const config = {
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keHFtemZ4YWxwc2t4dmp5bmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2OTczNzUsImV4cCI6MjA0MzI3MzM3NX0.h38Nvl2wVNZpeE50V-vjxqr6FwOdQMmU-PhJPinPsF8",
     SUPABASE_URL: "https://mdxqmzfxalpskxvjynco.supabase.co",
+    API_KEY: "82b1953f-af42-4e3a-9700-16444065f44b",
+    BASE_URL: "https://api.pokemontcg.io/v2",
 };
 
 // Initialisation de Supabase
